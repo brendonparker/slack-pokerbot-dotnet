1d69095a4db68844cd714fe29a7047f4ac21b04f
Brendon Parker <bparker@icsfl.com>
2020-08-13 09:52:11 -0400

    Add Newtonsoft.Json. Convert slack event.

--- Function.cs
+++ Function.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -14,6 +16,15 @@ namespace slack_pokerbot_dotnet
 {
     public class Functions
     {
+        private readonly AmazonDynamoDBClient client;
+        private readonly DynamoDBContext context;
+
+        public Functions()
+        {
+            client = new AmazonDynamoDBClient();
+            context = new DynamoDBContext(client);
+        }
+
         /// <summary>
         /// A Lambda function to respond to HTTP Post methods from API Gateway
         /// </summary>
@@ -23,6 +34,11 @@ namespace slack_pokerbot_dotnet
         {
             context.Logger.LogLine("Post Request\n");
 
+            var slackEvent = SlackEvent.FromFormEncodedData(request.Body);
+
+            if (!IsValidSlackToken(slackEvent.token))
+                throw new Exception("Invalid Slack Token");
+
             var response = new APIGatewayProxyResponse
             {
                 StatusCode = (int)HttpStatusCode.OK,
@@ -31,6 +47,13 @@ namespace slack_pokerbot_dotnet
             };
 
             return response;
+        }
+
+        private bool IsValidSlackToken(string token)
+        {
+            var configuredSlackToken = Environment.GetEnvironmentVariable("SlackToken");
+            var validTokens = configuredSlackToken.Split(new[] { ' ', ';', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            return validTokens.Contains(token);
         }
     }
 }
---
+++ SlackEvent.cs
@@ -0,0 +1,40 @@
+﻿
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Web;
+
+namespace slack_pokerbot_dotnet
+{
+    public class SlackEvent
+    {
+        public static SlackEvent FromFormEncodedData(string formEncodedData)
+        {
+            var decodedBody = HttpUtility.UrlDecode(formEncodedData);
+            var qs = HttpUtility.ParseQueryString(decodedBody);
+            var keyValuePairs = new Dictionary<string, string>();
+
+            foreach (var key in qs.AllKeys)
+            {
+                keyValuePairs[key] = qs[key];
+            }
+
+            // Hacky conversion 
+            return JsonConvert.DeserializeObject<SlackEvent>(JsonConvert.SerializeObject(keyValuePairs));
+        }
+
+        public string token { get; set; }
+        public string team_id { get; set; }
+        public string team_domain { get; set; }
+        public string enterprise_id { get; set; }
+        public string enterprise_name { get; set; }
+        public string channel_id { get; set; }
+        public string channel_name { get; set; }
+        public string user_id { get; set; }
+        public string user_name { get; set; }
+        public string command { get; set; }
+        public string text { get; set; }
+        public string response_url { get; set; }
+        public string trigger_id { get; set; }
+        public string api_app_id { get; set; }
+    }
+}
